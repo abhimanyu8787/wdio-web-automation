a8056ec5ff400f13e64e7537b8ae6c87ffd16b30
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-06-18 23:10:19 +0530

    added locators for method enterJourneyDetails

--- test/pageobjects/makemytrip/railway-booking-page.js
+++ test/pageobjects/makemytrip/railway-booking-page.js
@@ -14,6 +14,21 @@ class railwayBookingPage{
     get suggestion(){
         return $('//li[@class="react-autosuggest__suggestion react-autosuggest__suggestion--first"]')
     }
+    get datesOnCalendar(){
+        return $$('//div[@class="DayPicker-Week"]/div')
+    }
+    get calendar(){
+        return $('//span[@data-cy="travelDatelabel"]/parent::label/parent::div')
+    }
+    get class(){
+        return $('//span[@data-cy="class"]/parent::label/parent::div')
+    }
+    get classDropdown(){
+        return $$('//ul[@class="travelForPopup"]/li')
+    }
+    get submitButton(){
+        return $('//a[@data-cy="submit"]')
+    }
 
     async enterJourneyDetails(from, to, date, coachClass){
         await this.fromLocation.click()
@@ -24,6 +39,23 @@ class railwayBookingPage{
         await this.toTextBox.setValue(to)
         await browser.pause(2000)
         await this.suggestion.click()
+        await browser.pause(2000)
+        await this.calendar.click()
+        this.datesOnCalendar.forEach(async element => {
+            if(await element.getText()==date){
+                await element.click()
+                return
+            }
+        });
+        await browser.pause(2000)
+        await this.class.click()
+        this.classDropdown.forEach(async element => {
+            if(await element.getText()==coachClass){
+                await element.click()
+                return
+            }
+        })
+        await this.submitButton.click()
     }
 }
 
