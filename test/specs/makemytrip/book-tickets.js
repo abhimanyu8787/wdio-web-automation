aec6a4537bf19b4ed4bafda7844bc5ae2fc94a40
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-06-18 23:12:00 +0530

    added steps to enter passenger details

--- test/specs/makemytrip/book-tickets.js
+++ test/specs/makemytrip/book-tickets.js
@@ -1,13 +1,19 @@
 const homepage = require('../../pageobjects/makemytrip/home-page')
 const railwaybookingpage = require('../../pageobjects/makemytrip/railway-booking-page')
+const railwaylistingpage = require('../../pageobjects/makemytrip/railway-trains-listing-page')
+const railwayticketbooking = require('../../pageobjects/makemytrip/railway-ticket-booking-page')
 
-describe('Makemytrip ticket booking functionality', ()=>{
+describe('Makemytrip ticket booking functionality', function(){
+    this.retries(2)
     it('Book Railway Tickets End 2 End', async ()=>{
-        await browser.url('https://www.makemytrip.com/')
         await browser.maximizeWindow()
+        await browser.url('/')
         await homepage.closePopUpAd()
         await homepage.trainTicketsBooking.click()
-        await railwaybookingpage.enterJourneyDetails('Gwalior', 'Patna', 'Dummy', 'AC')
-        await browser.pause(5000)
+        await railwaybookingpage.enterJourneyDetails('Gwalior', 'Jhansi', '23', 'All Class')
+        await railwaylistingpage.selectCoachClassByIndex(0)
+        await railwayticketbooking.selectRefundOptionByIndex(1)
+        await railwayticketbooking.enterPassengerDetails('Mahi Sharma', '25', 'Female', 'Indian', 'Lower')
+        await railwayticketbooking.enterPassengerContactInformation('mahisharma@gmail.com','1111111111', '208001', 'Uttar Pradesh', 'Mahi Mahal')
     })
 })
